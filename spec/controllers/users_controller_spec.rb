fd908b8845a25d08a55cf623be75f98a3dda14de
emikod@mac.com <emikod@mac.com>
2016-07-17 01:55:48 -0600

    One test is failing

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  let (:user1) { create(:user) }
+  let (:user) { create(:user) }
 
   describe "GET #show" do
 
     before do
-      user = User.create!(username: "user2", email: "user2@blocmetrics.com", password: "password", password_confirmation: "password")
-      sign_in @user
+      sign_in user
     end
 
     it "returns http success" do
@@ -16,5 +15,4 @@ RSpec.describe UsersController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
 end
