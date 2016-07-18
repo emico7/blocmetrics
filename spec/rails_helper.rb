b985734d3f1b8b41a2ce344b15e2dfe554aec327
emikod@mac.com <emikod@mac.com>
2016-07-17 18:11:48 -0600

    All tests pass & a few more files deleted.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,7 +58,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryGirl::Syntax::Methods
-  config.include Devise::Test::ControllerHelpers
-  # config.include Devise::TestHelpers, type: :view
+  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::TestHelpers, type: :view
   config.include ControllerHelpers, :type => :controller
 end
